70e8d6527d532b79f38dde001c0c077ffb14f366
Diego Ochoa <daochoa@pucp.edu.pe>
2024-05-10 20:35:22 -0500

    fix sp

--- MiTutor/DataAccess/StoredProcedure.cs
+++ MiTutor/DataAccess/StoredProcedure.cs
@@ -2,7 +2,6 @@
 {
     public static class StoredProcedure
     {
-        public const string CREAR_ESPECIALIDAD = "ESP_CrearEspecialidad_Insert";
         public const string LISTAR_ESPECIALIDAD = "ESP_ListarEspecialidades_Select";
         public const string ACTUALIZAR_ESPECIALIDAD = "ESP_ActualizarEspecialidad_Update";
         public const string LISTAR_PLAN_ACTIONS = "sp_GetActionPlans";
