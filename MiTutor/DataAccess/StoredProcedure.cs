49fc734615a144b7c17fc612286d4b5c629cd101
maria <maria.rojasc@pucp.edu.pe>
2024-05-25 19:18:28 -0500

    Add procedure de actions plans alumno

--- MiTutor/DataAccess/StoredProcedure.cs
+++ MiTutor/DataAccess/StoredProcedure.cs
@@ -4,15 +4,20 @@
     {
         public const string LISTAR_ESPECIALIDAD = "ESP_ListarEspecialidades_Select";
         public const string ACTUALIZAR_ESPECIALIDAD = "ESP_ActualizarEspecialidad_Update";
+
+        //PLAN DE ACCIÓN
         public const string LISTAR_PLAN_ACTIONS = "sp_GetActionPlans";
         public const string CREAR_ACTION_PLAN = "sp_InsertActionPlan";
         public const string OBTENER_ACTION_PLAN_X_ID = "sp_GetActionPlansById";
+        public const string ACTUALIZAR_PLAN = "sp_UpdateActionPlan";
+        public const string LISTAR_ACTION_PLAN_ALUMNO = "sp_GetActionPlansStudent";
+        public const string ELIMINAR_ACTION_PLAN = "sp_DeleteActionPlan";
+
+        //COMPROMISOS
         public const string LISTAR_COMMITMENT_X_ID_ACTION_PLAN = "sp_GetCommitmentsByActionPlanId";
         public const string INSERTAR_COMMITMENT = "sp_InsertCommitment";
-        public const string ACTUALIZAR_PLAN = "sp_UpdateActionPlan";
         public const string ACTUALIZAR_COMMITMENT = "sp_UpdateCommitment";
-        public const string DELETE_COMMITMENT = "sp_DeleteCommitment";
-        public const string ELIMINAR_ACTION_PLAN = "sp_DeleteActionPlan";
+        public const string DELETE_COMMITMENT = "sp_DeleteCommitment";     
 
         //PERSONA
         public const string CREAR_PERSONA = "PERSON_INSERTAR_INSERT";
