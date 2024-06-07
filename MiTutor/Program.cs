4fe249bfcc15bf83699b13b46c0435586e78e937
Diego Ochoa <daochoa@pucp.edu.pe>
2024-06-07 14:47:33 -0500

    fix config

--- MiTutor/Program.cs
+++ MiTutor/Program.cs
@@ -12,12 +12,7 @@ using Serilog.Events;
 var builder = WebApplication.CreateBuilder(args);
 
 if (builder.Environment.IsProduction())
-{
-    builder.WebHost.ConfigureKestrel((context, options) =>
-    {
-        options.Configure(context.Configuration.GetSection("Kestrel"));
-    });
-    
+{    
     Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console()
