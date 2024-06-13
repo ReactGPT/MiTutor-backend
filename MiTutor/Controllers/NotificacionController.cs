675235833165326246065d1fdb69282b4f06baf2
Fernando <fernando24311@outlook.es>
2024-06-13 01:04:04 -0500

    servicio de listado de notificaciones

---
+++ MiTutor/Controllers/NotificacionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiTutor.Models;
+using MiTutor.Models.TutoringManagement;
+using MiTutor.Services;
+
+namespace MiTutor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificacionController : ControllerBase
+    {
+
+        private readonly ILogger<NotificacionController> _logger;
+        private readonly NotificacionService _notificacionService;
+
+        public NotificacionController(ILogger<NotificacionController> logger, NotificacionService notificacionService)
+        {
+            _logger = logger;
+            _notificacionService = notificacionService;
+        }
+
+        [HttpGet("/listarNotificaciones")]
+        public async Task<IActionResult> ListarNotificaciones([FromQuery] int userAcountId) 
+        {
+            List<Notificacion> notificaciones;
+
+            try
+            {
+                notificaciones = await _notificacionService.ListarNotificacionesPorUserAcount(userAcountId);
+                return Ok(new { success = true, data = notificaciones });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ERROR en controller", ex);
+            }
+
+        }
+
+    }
+}
---
+++ MiTutor/Models/Notificacion.cs
@@ -0,0 +1,9 @@
+﻿namespace MiTutor.Models
+{
+    public class Notificacion
+    {
+        public string resumen { get; set; }
+        public string descripcion { get; set; }
+        public string tipo { get; set; }
+    }
+}
